8e8c8f220f3e48d52af5f30b45b93e4d5953ff53
Daniel Norris <dnorris443@gmail.com>
2024-06-28 11:48:53 +0100

    fix(forge16/20): make battle spectate command work

--- forge20/src/main/java/com/envyful/battle/enforcement/command/BattleSpectateCommand.java
+++ forge20/src/main/java/com/envyful/battle/enforcement/command/BattleSpectateCommand.java
@@ -11,18 +11,25 @@ import com.envyful.api.forge.command.completion.player.PlayerTabCompleter;
 import com.envyful.api.forge.player.ForgeEnvyPlayer;
 import com.envyful.api.platform.PlatformProxy;
 import com.envyful.api.text.Placeholder;
-import com.envyful.battle.enforcement.config.BattleType;
 import com.pixelmonmod.pixelmon.api.battles.BattleResults;
+import com.pixelmonmod.pixelmon.api.battles.BattleType;
 import com.pixelmonmod.pixelmon.api.events.battles.BattleEndEvent;
 import com.pixelmonmod.pixelmon.api.storage.PlayerPartyStorage;
 import com.pixelmonmod.pixelmon.api.storage.StorageProxy;
+import com.pixelmonmod.pixelmon.api.util.helpers.NetworkHelper;
 import com.pixelmonmod.pixelmon.battles.BattleRegistry;
 import com.pixelmonmod.pixelmon.battles.api.BattleBuilder;
+import com.pixelmonmod.pixelmon.battles.api.rules.BattleRuleRegistry;
 import com.pixelmonmod.pixelmon.battles.api.rules.teamselection.TeamSelectionRegistry;
+import com.pixelmonmod.pixelmon.battles.controller.participants.PixelmonWrapper;
 import com.pixelmonmod.pixelmon.battles.controller.participants.PlayerParticipant;
 import com.pixelmonmod.pixelmon.battles.controller.participants.Spectator;
+import com.pixelmonmod.pixelmon.client.gui.battles.PixelmonClientData;
+import com.pixelmonmod.pixelmon.comm.packetHandlers.battles.*;
+import com.pixelmonmod.pixelmon.command.impl.SpectateCommand;
 import net.minecraft.commands.CommandSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Command(
@@ -44,6 +51,20 @@ public class BattleSpectateCommand {
             PlatformProxy.sendMessage(source, List.of("&c&l(!) &cThat player is not currently in a battle"));
             return;
         }
+
+        var watchedPlayer = battle.getPlayer(target.getParent());
+
+        NetworkHelper.sendPacket(new StartBattlePacket(battle.battleIndex, battle.getBattleType(watchedPlayer), battle.rules), source.getParent());
+        NetworkHelper.sendPacket(new SetAllBattlingPokemonPacket(PixelmonClientData.convertToGUI(Arrays.asList(watchedPlayer.allPokemon)), true), source.getParent());
+        List<PixelmonWrapper> teamList = watchedPlayer.getTeamPokemonList();
+        NetworkHelper.sendPacket(new SetBattlingPokemonPacket(teamList), source.getParent());
+        NetworkHelper.sendPacket(new SetPokemonBattleDataPacket(PixelmonClientData.convertToGUI(teamList), false), source.getParent());
+        NetworkHelper.sendPacket(new SetPokemonBattleDataPacket(watchedPlayer.getOpponentData(), true), source.getParent());
+        if (battle.getTeam(watchedPlayer).size() > 1) {
+            NetworkHelper.sendPacket(new SetPokemonTeamDataPacket(watchedPlayer.getAllyData()), source.getParent());
+        }
+
+        NetworkHelper.sendPacket(new StartSpectatePacket(target.getUniqueId(), battle.rules.getOrDefault(BattleRuleRegistry.BATTLE_TYPE)), source.getParent());
 
         battle.addSpectator(new Spectator(source.getParent(), target.getName()));
         PlatformProxy.sendMessage(source, List.of("&a&l(!) &aYou are now spectating " + target.getName()));
